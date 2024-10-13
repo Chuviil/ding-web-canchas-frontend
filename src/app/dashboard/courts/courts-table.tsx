17fc9edc434dfc686f9121cb5837009c51b179a4
Sebastian Quito <josue.quito@udla.edu.ec>
2024-10-13 11:33:38 -0500

    Basic LogIn TODO: - Use a base url on .env - Fix types errors

--- src/app/dashboard/courts/courts-table.tsx
+++ src/app/dashboard/courts/courts-table.tsx
@@ -70,30 +70,33 @@ const CourtsTable: React.FC<CourtsTableProps> = ({data}) => {
     });
 
     return (
-        <table className="min-w-full border-collapse table-auto">
-            <thead>
-            {table.getHeaderGroups().map(headerGroup => (
-                <tr key={headerGroup.id}>
-                    {headerGroup.headers.map(header => (
-                        <th key={header.id} className="border-b p-4">
-                            {flexRender(header.column.columnDef.header, header.getContext())}
-                        </th>
-                    ))}
-                </tr>
-            ))}
-            </thead>
-            <tbody>
-            {table.getRowModel().rows.map(row => (
-                <tr key={row.id}>
-                    {row.getVisibleCells().map(cell => (
-                        <td key={cell.id} className="border-b p-4">
-                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                        </td>
-                    ))}
-                </tr>
-            ))}
-            </tbody>
-        </table>
+        <div>
+            <button className={"bg-gray-900 text-white rounded px-4 py-2"} onClick={() => {router.push("/api/auth/signout")}}>Cerrar sesion</button>
+            <table className="min-w-full border-collapse table-auto">
+                <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                            <th key={header.id} className="border-b p-4">
+                                {flexRender(header.column.columnDef.header, header.getContext())}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                            <td key={cell.id} className="border-b p-4">
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
